20cf47c99f92976754e70bbbafa03331fdb96cea
Farzana Talukder Sumona <94677684+farzanasumona@users.noreply.github.com>
2024-07-07 16:21:52 +0200

    Update P13_fibonacci_sequence.py

--- P13_fibonacci_sequence.py
+++ P13_fibonacci_sequence.py
@@ -1,20 +1,15 @@
 # Write a program that generates a Fibonacci sequence of length `n`.
-n = int(input("Enter a number: "))
 
-Previous = 0
+n = int(input("Enter a positive number: "))
+
+previous = 0
 current = 1
 
-if n <= 0:
-    result = []
-elif n == 1:
-    result = [Previous]
-else:
-    result = [Previous, current]
+print(previous)
+print(current)
 
-for i in range(2, n):
-  f_sum = Previous + current
-  result.append(f_sum)
-  Previous = current
+for i in range(3, n+1):
+  f_sum = previous + current
+  print(f_sum)
+  previous = current
   current = f_sum
-print(result)
-
